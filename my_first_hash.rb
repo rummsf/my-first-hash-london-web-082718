b4d12789fc4aea5bda292009f676f1ceace612c8
Irum F <irummf@gmail.com>
2018-08-27 00:40:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,9 +37,9 @@ shipping_manifest = ["muskets" => 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest = ["gunpowder" => 4]
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
